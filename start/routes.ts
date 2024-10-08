34170f2543d7b5d7e5b7b33584cfcbdc96a94149
Jesus-Aranda-Rdz <jesus_aranda_rodriguez@hotmail.com>
2024-10-07 20:38:02 -0600

    [ADD] routes auth

--- start/routes.ts
+++ start/routes.ts
@@ -1,34 +1,14 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('login', 'AuthController.login');
   Route.post('logout', 'AuthController.logout');
   Route.resource('users', 'UsersController').apiOnly();
-}).prefix('api/v1').namespace('App/Controllers/Http/Authentication');
+}).prefix('api/v1').namespace('App/Controllers/Http/Authentication').middleware('auth');
 
 
 Route.group(() => {
   Route.resource('chats', 'ChatsController').apiOnly().except(['update', 'show']);
   Route.resource('messages', 'MessagesController').apiOnly().except(['index']);
-}).prefix('api/v1').namespace('App/Controllers/Http/ChatSystem');
+}).prefix('api/v1').namespace('App/Controllers/Http/ChatSystem').middleware('auth');
 
